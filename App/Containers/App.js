7d6c0eed2172ea57d8c8b8b71e83b4a860212a61
Christophe Wagner <chris.wagn22@gmail.com>
2019-02-25 16:02:10 +0700

    Init

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,12 +1,29 @@
-import '../Config'
-import DebugConfig from '../Config/DebugConfig'
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import RootContainer from './RootContainer'
-import createStore from '../Redux'
+import "../Config";
+import DebugConfig from "../Config/DebugConfig";
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import RootContainer from "./RootContainer";
+import createStore from "../Redux";
+import DatabaseProvider from "@nozbe/watermelondb/DatabaseProvider";
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+
+import { mySchema } from "../Models/schema";
+import Blog from "../Models/Blog";
 
 // create our store
-const store = createStore()
+const store = createStore();
+
+// WatermelonDB
+const adapter = new SQLiteAdapter({
+  dbName: "test",
+  schema: mySchema
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [Blog]
+});
 
 /**
  * Provides an entry point into our application.  Both index.ios.js and index.android.js
@@ -18,16 +35,16 @@ const store = createStore()
  * We separate like this to play nice with React Native's hot reloading.
  */
 class App extends Component {
-  render () {
+  render() {
     return (
       <Provider store={store}>
-        <RootContainer />
+        <DatabaseProvider database={database}>
+          <RootContainer database={database} />
+        </DatabaseProvider>
       </Provider>
-    )
+    );
   }
 }
 
 // allow reactotron overlay for fast design in dev mode
-export default DebugConfig.useReactotron
-  ? console.tron.overlay(App)
-  : App
+export default (DebugConfig.useReactotron ? console.tron.overlay(App) : App);
--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,30 +1,45 @@
-import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
-import ReduxNavigation from '../Navigation/ReduxNavigation'
-import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
+import React, { Component } from "react";
+import { View, StatusBar } from "react-native";
+import ReduxNavigation from "../Navigation/ReduxNavigation";
+import { connect } from "react-redux";
+import StartupActions from "../Redux/StartupRedux";
 
 // Styles
-import styles from './Styles/RootContainerStyles'
+import styles from "./Styles/RootContainerStyles";
 
 class RootContainer extends Component {
-  componentDidMount () {
-    this.props.startup()
+  async generate() {
+    const { database, startup } = this.props;
+    await database.unsafeResetDatabase();
+    const blog = await database.collections.get("blogs").create(blog => {
+      blog.name = "test";
+    });
+
+    startup(blog);
   }
 
-  render () {
+  componentDidMount() {
+    this.props.startup();
+
+    this.generate();
+  }
+
+  render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <ReduxNavigation />
       </View>
-    )
+    );
   }
 }
 
 // wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
-})
+const mapDispatchToProps = dispatch => ({
+  startup: blog => dispatch(StartupActions.startup(blog))
+});
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+export default connect(
+  null,
+  mapDispatchToProps
+)(RootContainer);
